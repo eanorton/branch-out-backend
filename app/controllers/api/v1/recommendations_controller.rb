e0b37ce2c990fe467692bb7151ced92d0616f8db
eanorton <edwardnorton@icloud.com>
2018-07-20 10:43:00 -0400

    fetches to replace top tracks on click

--- app/controllers/api/v1/recommendations_controller.rb
+++ app/controllers/api/v1/recommendations_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::RecommendationsController < ApplicationController
   def search
 
     @user = User.find_by(username: params[:u])
-
     @user.refresh_access_token
 
     header = {
@@ -18,7 +17,6 @@ class Api::V1::RecommendationsController < ApplicationController
     recommendation_search = RestClient.get("https://api.spotify.com/v1/artists/#{searched_artist_id}/related-artists", header)
     recommendation_results = JSON.parse(recommendation_search.body)
 
-
     searched_artist_top_tracks = RestClient.get("https://api.spotify.com/v1/artists/#{searched_artist_id}/top-tracks?country=US", header)
     searched_artist_top_tracks_resp = JSON.parse(searched_artist_top_tracks.body)
 
@@ -32,14 +30,12 @@ class Api::V1::RecommendationsController < ApplicationController
     artist4_top_tracks_resp = JSON.parse(artist4_top_tracks.body)
 
     artists_object = {
-
       searched_artist: artist_response,
       recommended_artists: recommendation_results,
       searched_artist_tracks: searched_artist_top_tracks_resp,
       rec1_artist_tracks: artist2_top_tracks_resp,
       rec2_artist_tracks: artist3_top_tracks_resp,
       rec3_artist_tracks: artist4_top_tracks_resp
-
     }
 
     render json: artists_object
@@ -49,7 +45,6 @@ class Api::V1::RecommendationsController < ApplicationController
   def search_on_click
 
     @user = User.find_by(username: params[:u])
-
     @user.refresh_access_token
 
     header = {
@@ -57,11 +52,22 @@ class Api::V1::RecommendationsController < ApplicationController
     }
 
     recommendation_search = RestClient.get("https://api.spotify.com/v1/artists/#{params[:q]}/related-artists?limit=3", header)
-
     recommendation_results = JSON.parse(recommendation_search.body)
 
+    artist1_top_tracks = RestClient.get("https://api.spotify.com/v1/artists/#{recommendation_results["artists"][0]["id"]}/top-tracks?country=US", header)
+    artist1_top_tracks_resp = JSON.parse(artist1_top_tracks.body)
+
+    artist2_top_tracks = RestClient.get("https://api.spotify.com/v1/artists/#{recommendation_results["artists"][1]["id"]}/top-tracks?country=US", header)
+    artist2_top_tracks_resp = JSON.parse(artist2_top_tracks.body)
+
+    artist3_top_tracks = RestClient.get("https://api.spotify.com/v1/artists/#{recommendation_results["artists"][2]["id"]}/top-tracks?country=US", header)
+    artist3_top_tracks_resp = JSON.parse(artist3_top_tracks.body)
+
     artists_object = {
-      recommended_artists: recommendation_results
+      recommended_artists: recommendation_results,
+      rec1_artist_tracks: artist1_top_tracks_resp,
+      rec2_artist_tracks: artist2_top_tracks_resp,
+      rec3_artist_tracks: artist3_top_tracks_resp
     }
 
     render json: artists_object
